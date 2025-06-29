0f542cc428bcd349370b9a6ca07c0de8e8fe2b40
shahdibrahim2002 <shahdibrahim1234567890@gmail.com>
2025-06-29 11:02:36 +0300

    loginn

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ForgotPassword } from "./features/forgot-password/forgot-password";
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
