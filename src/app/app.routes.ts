acea215eef52ca430f59dbb8159d2c01409dd9a4
Mariam Ashraf <your.email@example.com>
2025-06-30 17:37:08 +0300

    image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { Register } from './features/auth/register/register';
-import { Home } from './features/home/home';
 
-export const routes: Routes = [
-    { path: '', redirectTo: 'register', pathMatch: 'full' },
-    { path: 'register', component: Register },
-    { path: 'home', component: Home },
-
-];
+export const routes: Routes = [];
