d0ddced4d14d3bc282228ab9e1e8dcec6eef94fb
shahdibrahim2002 <shahdibrahim1234567890@gmail.com>
2025-07-05 13:17:31 +0300

    Save local changes before pulling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,7 @@
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AdminComponent } from '../components/admin/admin'; // عدلي المسار حسب مكان الملف
-import { SpecializationDetails } from '../components/specialization-details/specialization-details';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'Specializations', pathMatch: 'full' },
-  { path: 'Specializations', component: AdminComponent },
-  { path: 'SpecializationDetails/:id', component: SpecializationDetails },
-  { path: 'SpecializationDetails/:id', component: AdminComponent} // تأكدي من استيراد المكون المناسب
-  // أضيفي هنا أي مسارات أخرى تحتاجينها
-];
-=======
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
-import { ForgotPassword } from './features/forgot-password/forgot-password';
+import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
 import { DoctorLayout } from './layouts/doctor-layout/doctor-layout';
 import { RoleGuard } from './guards/RoleGurad';
 import { Home } from './features/home/home';
@@ -22,6 +9,7 @@ import { AdminLayout } from './layouts/admin-layout/admin-layout';
 import { AdminDashboard } from './features/admin-dashboard/admin-dashboard';
 import { Calendar } from './features/doctor/calendar/calendar';
 import { AppointmentsByDate } from './features/doctor/appointments-by-date/appointments-by-date';
+import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
@@ -51,4 +39,4 @@ export const routes: Routes = [
 ]
 
     
->>>>>>> 8d6e5e9f63d415147212ceed0fd32ddf05f0b5b5
+
